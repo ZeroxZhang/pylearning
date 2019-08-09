861f95330f8bfd6f3bf67edc2379408dfb847a36
zeroxzhang <zeroxhku@gmail.com>
2019-08-09 23:26:23 +0800

    commit

--- mydic.py
+++ mydic.py
@@ -1,8 +1,16 @@
-mydic={"iphone":100,"huawei":1200}
-print(mydic)
-ip=(mydic["iphone"])
-print("iphone's price:"+str(ip))
-mydic["iphone"]=5
-print(mydic)
-mydic.clear()
-print(mydic)
+# mydic={"iphone":100,"huawei":1200}
+# # print(mydic)
+# ip=(mydic["iphone"])
+# print("iphone's price:"+str(ip))
+# mydic["iphone"]=5
+# print(mydic)
+# mydic.clear()
+# print(mydic)
+
+age= int(input("please input your age: "))
+if age<21:
+    print("you can not smoke")
+elif age==21:
+    print("you are legal")
+else:
+    print("you can smoke")
