acc28bb0a7ff62310a6313189aafc5bf585f728a
Zerox Zhang <zeroxhku@gmail.com>
2019-08-09 18:27:48 +0800

    commit

--- listcomprehension.py
+++ listcomprehension.py
@@ -11,9 +11,15 @@ print(newlist)
 #列表解析式写法
 print([i*2 for i in range(11)])
 
-#需求二：从名字列表中，查找姓张的
+#需求二：从名字列表中，查找姓张的,并放在新的list中
 
 namelist = ["张一","张二","王3","王4"]
+'''复杂写法'''
+emptylist =[]
 for name in namelist:
     if name.startswith("张"):
-        print(name)
+        emptylist.append(name)
+print(emptylist)
+
+'''列表解析式写法'''
+print([name for name in namelist if name.startswith("张")])
